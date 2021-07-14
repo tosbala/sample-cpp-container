c0863fa5b423732282492964d9751705dfbca264
Balamurugan Seenivasan <balamurugan.s@imanage.com>
2021-07-14 12:13:50 +0530

    Initial Commit

---
+++ src/factorial.cpp
@@ -0,0 +1,10 @@
+#include "factorial.h"
+
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "factorial.h"
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello Factorial!!!" << std::endl;
+    std::cout << 4 << "!" << " = " << Factorial(4) << std::endl;
+    return 0;
+}
